56dd5e162a03d74a729cae8aa6786d75dd3f1493
Ренат Хасанов <Tyuiu.KhasanovRV@outlook.com>
2023-11-17 21:01:44 +0500

    Add project files.

---
+++ Tyuiu.KhasanovRV.Sprint5.Task0.V18.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.KhasanovRV.Sprint5.Task0.V18.Lib
+{
+    public class DataService : ISprint5Task0V18
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask0.txt";
+
+            double y = x / (Math.Sqrt(Math.Pow(x, 2) + x));
+
+            y = Math.Round(y, 3, MidpointRounding.AwayFromZero);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
---
+++ Tyuiu.KhasanovRV.Sprint5.Task0.V18.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KhasanovRV.Sprint5.Task0.V18.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KhasanovRV.Sprint5.Task0.V18.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d7830f7-d010-4db8-a838-a360f62f0605")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
